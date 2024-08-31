355683ed2aaa4315a4c862d2ccf07109c8092e75
Samuel0322M <marinsanchezs040@gmail.com>
2024-08-31 17:02:23 -0500

    Clase 15

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 #flask con f minuscula es la extension, Flask es la clase que nos permite crear instancias de flask
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, abort
+from flask_bootstrap import Bootstrap5
 
 #crear una nueva instancia de flask declaramos una variable llamada app y mandar llamar la clase flask para crear una nueva instancia
 app = Flask(__name__)
+#de esta forma se inicializa bootstrap
+Bootstrap = Bootstrap5(app)
 app.debug = "true"
 todos = ["Comprar chaqueta", "Sonic Frontiers", "Mando xbox"]
+
+@app.route('/error')
+def internal_error():
+    abort(500)
+
+@app.errorhandler(500)
+def system_error(error):
+    return render_template('error.html', error=error)
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
 
 @app.route("/")
 def index():
