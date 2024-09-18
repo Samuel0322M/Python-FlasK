bd18043be51c47f95e164cb357c36c72e4e189d2
Samuel0322M <marinsanchezs040@gmail.com>
2024-09-17 20:48:19 -0500

    clase 31

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,10 @@
 from flask import render_template, redirect, flash, url_for, session
 from flask_login import login_user, login_required, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from . import auth
 from app.forms import loginForm
-from app.firestore_service import get_user
+from app.firestore_service import get_user, user_put
 from app.models import UserModel, UserData
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -37,6 +38,39 @@ def login():
 
     
     return render_template('login.html', **context)
+
+@auth.route('signup', methods=['GET', 'POST'])
+def signup():
+    signup_form= loginForm()
+    context = {
+        'signup_form': signup_form
+    }
+
+    if signup_form.validate_on_submit():
+        username = signup_form.username.data
+        password = signup_form.password.data
+
+        user_doc = get_user(username)
+
+        if user_doc.to_dict() is None:
+            password_hash = generate_password_hash(password)
+            user_data = UserData(username, password_hash)
+            user_put(user_data)
+
+            if check_password_hash or password (user_doc.to_dict()['password'], password):
+                user_data = UserData(username,password)
+                user = UserModel(user_data)
+
+            login_user(user)
+            flash('bienvenido!')
+            return redirect(url_for('hello'))
+        
+        else:
+            flash('El usuario Existe!')
+
+    return render_template('signup.html', **context )
+
+
 
 @auth.route('logout')
 @login_required
